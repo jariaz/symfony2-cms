ba7050f444cda4f3b68a519e85b5f5badd36236e
Joseph Ariaz <josephariaz@gmail.com>
2015-05-06 21:06:30 -0700

    small updates

--- src/Custom/CMSBundle/Form/UserType.php
+++ src/Custom/CMSBundle/Form/UserType.php
@@ -16,7 +16,7 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('password')
+            ->add('password', 'password')
         ;
     }
     
