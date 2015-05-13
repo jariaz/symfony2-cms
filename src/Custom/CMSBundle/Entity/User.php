6dfd81a8b21f82c0834d0bba183ac7c180724208
Joseph Ariaz <josephariaz@gmail.com>
2015-05-12 22:04:38 -0700

    This took me WAY too long

--- src/Custom/CMSBundle/Entity/User.php
+++ src/Custom/CMSBundle/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Table()
  * @ORM\Entity
  */
-class User
+class User implements UserInterface
 {
     /**
      * @var integer
@@ -32,7 +32,7 @@ class User
     /**
      * @var string
      *
-     * @ORM\Column(name="password", type="string", length=255)
+     * @ORM\Column(name="password", type="string", length=64)
      */
     private $password;
 
