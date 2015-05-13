1686a0a09e468ac611ba8cdcdc1ff7857f033411
Joseph Ariaz <josephariaz@gmail.com>
2015-05-12 22:03:41 -0700

    Small stuff, swapped the env for vagrant

--- web/app.php
+++ web/app.php
@@ -18,7 +18,10 @@ $apcLoader->register(true);
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+//$kernel = new AppKernel('prod', false);
+
+//Set just for testing...
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
